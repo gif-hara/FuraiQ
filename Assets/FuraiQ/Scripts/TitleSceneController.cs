57c3ca58d9f966fd87e2230da5335812d1899bba
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-07 20:33:51 +0900

    Add new assets and scripts for UI Toolkit integration

--- Assets/FuraiQ/Scripts/TitleSceneController.cs
+++ Assets/FuraiQ/Scripts/TitleSceneController.cs
@@ -1,9 +1,19 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
 namespace FuraiQ
 {
     /// <summary>
     /// 
     /// </summary>
-    public sealed class TitleSceneController
+    public sealed class TitleSceneController : MonoBehaviour
     {
+        [SerializeField]
+        private UIDocument rootUIPrefab;
+
+        void Start()
+        {
+            Instantiate(rootUIPrefab);
+        }
     }
 }
