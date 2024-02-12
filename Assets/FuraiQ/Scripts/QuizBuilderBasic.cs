71c3f362de30c39de10cbf5061b2552c0f67e766
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-12 22:04:44 +0900

    QuizBuilderBasic.csの変更：オプションの追加と表示の修正

--- Assets/FuraiQ/Scripts/QuizBuilderBasic.cs
+++ Assets/FuraiQ/Scripts/QuizBuilderBasic.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace FuraiQ
@@ -25,9 +26,13 @@ namespace FuraiQ
 
         public override IQuiz Build()
         {
-            var options = new List<QuizOption>
+            var options = new List<(string option, bool isCorrect)>
             {
-                new() { message = correctOptions.OrderBy(_ => Guid.NewGuid()).First(), isCorrect = true }
+                new()
+                {
+                    option = correctOptions.OrderBy(_ => Guid.NewGuid()).First(),
+                    isCorrect = true
+                }
             };
             var incorrectIndexies = Enumerable.Range(0, incorrectOptions.Count).OrderBy(_ => Guid.NewGuid()).ToList();
             for (int i = 0; i < optionNumber - 1; i++)
@@ -36,9 +41,24 @@ namespace FuraiQ
                 {
                     break;
                 }
-                options.Add(new QuizOption { message = incorrectOptions[incorrectIndexies[i]], isCorrect = false });
+                options.Add
+                (
+                    new()
+                    {
+                        option = incorrectOptions[incorrectIndexies[i]],
+                        isCorrect = false
+                    }
+                );
             }
-            return new Quiz(question, options.OrderBy(i => Guid.NewGuid()).ToList());
+            options = options.OrderBy(_ => Guid.NewGuid()).ToList();
+            var sb = new StringBuilder();
+            sb.AppendLine(question);
+            sb.AppendLine();
+            for (var i = 0; i < options.Count; i++)
+            {
+                sb.AppendLine($"{i + 1}. {options[i].option}");
+            }
+            return new Quiz(sb.ToString(), options.Select((x, index) => new QuizOption { message = (index + 1).ToString(), isCorrect = x.isCorrect }).ToList());
         }
     }
 }
